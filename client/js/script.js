16f695f92a3bcad221ceafeb67db2df9edbc0008
Jovan Lanutan <jolanutan@gmail.com>
2023-05-08 19:37:54 +0800

    change for deployment

--- client/js/script.js
+++ client/js/script.js
@@ -95,7 +95,7 @@ form.addEventListener('submit', (e) => {
   if (!location) {
     displayError('error');
   } else {
-    fetch(`http://localhost:3000/weather?address=${location}`)
+    fetch(`/weather?address=${location}`)
       .then((res) => res.json())
       .then((data) => {
         if (data.error) {
